f6f5eb4f875807f775832d4725d18bac67ea6174
SparrowNado <123578310+SparrowNado@users.noreply.github.com>
2023-11-19 03:10:22 +0800

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import truelogo from '../assets/truelogo.png'
-import background from '../assets/background.jpg'
+
 
 import Favicon from "react-favicon";
 
@@ -15,47 +15,36 @@ function Hero() {
     <div
     id="hero-section"
     className="relative w-screen h-screen text-white overflow-hidden z-0 bg-black-300"
-
   >
-        
-
-<Favicon url={truelogo} />
-
-    
-
-      {/* Content Overlay */}
-      <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50"></div>
-
-        {/*previous py-24  */}
-      {/* Content */}
-      <div className="relative container mx-auto flex px-12 py-48 text-white   align-middle items-center justify-center flex-col ">
-        <div className="text-center lg:w-5/12 w-full">
-          <h1 className="my-4 text-7xl font-bold leading-tight">
-            I am 
-          </h1>
-          <h1 className="my-4  text-6xl font-bold leading-tight  ">
-           Gabriel D. Ramoso
-          </h1>
-          <h2 className="text-3xl mb-8 font-bold">
-            An aspiring Front End / Web Developer
-          </h2>
-          <p className="text-2xl mb-8 font-bold">
-            Working hard everyday to get better at my craft
-          </p>
-      
-          <div className="flex justify-center mx-auto">
-            
-            <button className="hover:underline bg-white text-gray-800 font-bold rounded-full py-4 px-8  transform hover:scale-125 duration-300 no-underline">
-              <a href='https://github.com/SparrowNado'>
-                Check My Github
-              </a>
-              
-            </button>
-            
-          </div>
+    <Favicon url={truelogo} />
+  
+    {/* Content Overlay */}
+    <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50"></div>
+  
+    {/* Content */}
+    <div className="relative container mx-auto flex px-4 sm:px-8 py-16 sm:py-32 text-white sm:text-base align-middle items-center justify-center flex-col">
+      <div className="text-center lg:w-5/12 w-full">
+        <h1 className="my-2 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold leading-tight">
+          I am
+        </h1>
+        <h1 className="my-2 text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold leading-tight sm:leading-snug">
+          Gabriel D. Ramoso
+        </h1>
+        <h2 className="text-xl sm:text-2xl md:text-3xl mb-4 md:mb-6 lg:mb-8 font-bold">
+          An aspiring Front End / Web Developer
+        </h2>
+        <p className="text-lg sm:text-xl md:text-2xl mb-4 md:mb-6 lg:mb-8 font-bold">
+          Working hard every day to get better at my craft
+        </p>
+  
+        <div className="flex justify-center mx-auto">
+          <button className="hover:underline bg-white text-gray-800 font-bold rounded-full py-3 px-6 sm:py-4 sm:px-8 transform hover:scale-105 duration-300 no-underline">
+            <a href="https://github.com/SparrowNado">Check My Github</a>
+          </button>
         </div>
       </div>
     </div>
+  </div>
   );
 }
 
